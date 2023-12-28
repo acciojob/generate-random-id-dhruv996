3a0e17f8486bd7e5f4b4bd20587cec4ef576a45d
Dhruv Varshney <144780592+dhruv996@users.noreply.github.com>
2023-12-28 14:22:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-function makeid(l) {
-  // write your code here
+document.addEventListener('DOMContentLoaded', function () {
+  // Example usage
+  const result1 = makeid(5);
+  document.getElementById('result').innerText = `Random string of length 5: '${result1}'`;
+
+  const result2 = makeid(10);
+  document.getElementById('result').innerText += `\nRandom string of length 10: '${result2}'`;
+});
+
+function makeid(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+
+  return result;
 }
 
-// Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
